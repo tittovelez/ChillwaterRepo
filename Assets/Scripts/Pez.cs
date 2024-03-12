a4ab4608e1d0c237f279a85d083709aa49c5f25f
ALBERTVELEZ <albert.velez@enti.cat>
2024-03-12 14:25:54 +0100

    save 8

---
+++ Assets/Scripts/Pez.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoAutomatico : MonoBehaviour
+{
+    public GameObject ObjetoAmover;
+    public float velocidad;
+    public Transform StartPont;
+    public Transform EndPoint;
+    public Vector3 MoverHacia;
+    private int puntuacion; // Puntuación individual para cada objeto
+    public string tagRecoger = "Recoger"; // Tag del objeto para recoger
+
+    private SpriteRenderer spriteRenderer;
+    private PuntuacionTotal puntuacionTotalScript;
+
+    private void Start()
+    {
+        MoverHacia = EndPoint.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Encuentra el componente PuntuacionTotal en la escena
+        puntuacionTotalScript = FindObjectOfType<PuntuacionTotal>();
+
+        // Si no se encuentra, muestra un mensaje de error
+        if (puntuacionTotalScript == null)
+        {
+            Debug.LogError("No se encontró el objeto PuntuacionTotal en la escena.");
+        }
+
+        // Aquí puedes establecer diferentes puntuaciones para diferentes objetos
+        switch (gameObject.name)
+        {
+            case "Fish1":
+                puntuacion = 10;
+                break;
+            case "Fish2":
+                puntuacion = 10;
+                break;
+            case "Fish3":
+                puntuacion = 20;
+                break;
+            case "Fish4":
+                puntuacion = 40;
+                break;
+            case "Fish5":
+                puntuacion = -15;
+                break;
+            case "Fish6":
+                puntuacion = 40;
+                break;
+            case "Fish7":
+                puntuacion = 60;
+                break;
+            case "Fish8":
+                puntuacion = 80;
+                break;
+
+            case "Fish9":
+                puntuacion = -15;
+                break;
+            case "Fish10":
+                puntuacion = -15;
+                break;
+            // Agrega más casos según sea necesario para cada objeto
+            default:
+                puntuacion = 0;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        ObjetoAmover.transform.position = Vector3.MoveTowards(ObjetoAmover.transform.position, MoverHacia, velocidad * Time.deltaTime);
+
+        if (ObjetoAmover.transform.position == EndPoint.position)
+        {
+            MoverHacia = StartPont.position;
+        }
+        if (ObjetoAmover.transform.position == StartPont.position)
+        {
+            MoverHacia = EndPoint.position;
+        }
+        if (MoverHacia.x > transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Anzuelo"))
+        {
+            if (puntuacionTotalScript != null)
+            {
+                // Llama al método SumarPuntuacion() del objeto PuntuacionTotal y pasa su propia puntuación
+                puntuacionTotalScript.SumarPuntuacion(puntuacion);
+            }
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/Puntuacion Total.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement; // Necesario para trabajar con escenas
+
+public class PuntuacionTotal : MonoBehaviour
+{
+    public TextMeshProUGUI textoPuntuacion;
+    public int puntuacionParaGanar = 100; // Establece el umbral de puntuación para ganar
+    public string Victoria; // Nombre de la escena de victoria
+
+    private int puntuacionTotal = 0;
+
+    // Método para actualizar el texto de la puntuación
+    void ActualizarTextoPuntuacion()
+    {
+        if (textoPuntuacion != null)
+        {
+            textoPuntuacion.text = puntuacionTotal.ToString(); 
+        }
+    }
+
+    // Método para sumar la puntuación
+    public void SumarPuntuacion(int puntuacion)
+    {
+        puntuacionTotal += puntuacion;
+        ActualizarTextoPuntuacion();
+
+        // Verificar si la puntuación total alcanza el umbral para ganar
+        if (puntuacionTotal >= puntuacionParaGanar)
+        {
+            Ganar();
+        }
+    }
+
+    // Método para cargar la escena de victoria
+    void Ganar()
+    {
+        // Cargar la escena de victoria
+        if (!string.IsNullOrEmpty(Victoria))
+        {
+            SceneManager.LoadScene(Victoria);
+        }
+        else
+        {
+            Debug.LogWarning("Victoria.");
+        }
+    }
+}
