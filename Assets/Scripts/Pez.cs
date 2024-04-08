37330efce662f9e1062b0769469cb32e5cb6ee61
ALBERTVELEZ <albert.velez@enti.cat>
2024-04-08 14:26:54 +0200

    save 11

--- Assets/Scripts/Pez.cs
+++ Assets/Scripts/Pez.cs
@@ -9,65 +9,16 @@ public class MovimientoAutomatico : MonoBehaviour
     public Transform StartPont;
     public Transform EndPoint;
     public Vector3 MoverHacia;
-    private int puntuacion; // Puntuación individual para cada objeto
     public string tagRecoger = "Recoger"; // Tag del objeto para recoger
 
     private SpriteRenderer spriteRenderer;
-    private PuntuacionTotal puntuacionTotalScript;
 
     private void Start()
     {
         MoverHacia = EndPoint.position;
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        // Encuentra el componente PuntuacionTotal en la escena
-        puntuacionTotalScript = FindObjectOfType<PuntuacionTotal>();
-
         // Si no se encuentra, muestra un mensaje de error
-        if (puntuacionTotalScript == null)
-        {
-            Debug.LogError("No se encontró el objeto PuntuacionTotal en la escena.");
-        }
-
-        // Aquí puedes establecer diferentes puntuaciones para diferentes objetos
-        switch (gameObject.name)
-        {
-            case "Fish1":
-                puntuacion = 10;
-                break;
-            case "Fish2":
-                puntuacion = 10;
-                break;
-            case "Fish3":
-                puntuacion = 20;
-                break;
-            case "Fish4":
-                puntuacion = 40;
-                break;
-            case "Fish5":
-                puntuacion = 50;
-                break;
-            case "Fish6":
-                puntuacion = 40;
-                break;
-            case "Fish7":
-                puntuacion = 60;
-                break;
-            case "Fish8":
-                puntuacion = 80;
-                break;
-
-            case "Fish9":
-                puntuacion = 100;
-                break;
-            case "Fish10":
-                puntuacion = 100;
-                break;
-            // Agrega más casos según sea necesario para cada objeto
-            default:
-                puntuacion = 0;
-                break;
-        }
     }
 
     void Update()
@@ -91,8 +42,7 @@ public class MovimientoAutomatico : MonoBehaviour
             spriteRenderer.flipX = false;
         }
     }
-
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Anzuelo"))
         {
